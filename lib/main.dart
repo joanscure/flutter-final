5df2e94466941a1d5ab2a1e53ab60b3a1398ec80
corzzo75 <corzo.guerrero.1998@gmail.com>
2023-01-11 19:03:58 -0500

    before pull

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:projectomovilfinal/screens/receta.dart';
+import 'package:projectomovilfinal/screens/Receta/receta.dart';
 import 'package:projectomovilfinal/settings/constant.dart';
 
 void main() {
